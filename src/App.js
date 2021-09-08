a158124a1bcf01b9331c9570cab1700504fabe5b
Lawrence Buta <larry.buta@gmail.com>
2021-09-08 18:58:40 -0400

    Interim save to repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import Accordion from './components/Accordion';
+import React from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 
 const items = [
-    {
-        title: "What is React",
-        content: "React is a front end js framework"
-    },
-    {
-        title: "Who uses React",
-        content: "The software engineers at WSS"
-    },
-    {
-        title: "How do you use React?",
-        content: "You use React by creating components"
-    }
+  {
+    title: "What is React",
+    content: "React is a front end js framework",
+  },
+  {
+    title: "Who uses React",
+    content: "The software engineers at WSS",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
 ];
 
 export default () => {
-    return (
-        <div><Accordion items={items} /></div>
-    );
+  return (
+    <div>
+      <Search />
+    </div>
+  );
 };
